a8f692a75ff752395e963ada167e3520026ca8c6
StevenVC-P <77998885+StevenVC-P@users.noreply.github.com>
2023-07-11 21:45:56 -0500

    added support for battles

---
+++ src/components/Templates/Battles.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import "./Universal.css";
+import "./Battles.css";
+
+const Battles = (props) => {
+    const source = props.face || props.card;
+    const imageData = props.imageData;
+    const {name, mana_cost, oracle_text, type_line, set } = source;
+    return (
+        <div className="card-container">
+            <div className="battle-card-background card-background">
+                <div className="battle-card-frame">
+                    <div className="battle-frame-header">
+                        <h1 className="name">{name}</h1>
+                        {mana_cost}
+                    </div>
+                        <div className="battle-frame-image">
+                            {imageData && <img src={`data:image/png;base64,${imageData.image}`} alt="Generated" />}
+                        </div>
+                        <div className="battle-frame-type-line">
+                            <h1 className="type">{type_line}</h1>
+                            {set}
+                        </div>
+                        <div className="battle-frame-text-box">
+                            <p className="description ftb-inner-margin">{oracle_text}</p>
+                        </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Battles
