2bdf6e6e931269bf7f41dc378708be3b9670a4f2
StevenVC-P <77998885+StevenVC-P@users.noreply.github.com>
2023-08-12 13:51:04 -0500

    added more data and orgainization to image prompts

--- src/helpers/ImgDataFormatter.jsx
+++ src/helpers/ImgDataFormatter.jsx
@@ -1,31 +1,35 @@
 import axios from "axios";
 
-const generateImage = async (textPrompts, height, width) => {
-  const response = await axios.post(`http://localhost:5000/api/generate-image`, {
-    height: height, // height of the output image
-    width: width, // width of the output image
-    cfg_scale: 25, // replace with the desired cfg_scale
-    clip_guidance_preset: 'NONE', // replace with the desired clip_guidance_preset
-    // sampler: 'K_DPM_2_ANCESTRAL', // replace with the desired sampler
-    samples: 1, // replace with the desired number of samples
-    steps: 75, // replace with the desired number of steps
-    style_preset: 'digital-art', // replace with the desired style_preset
-    text_prompts: textPrompts.map((text, index) => ({
-      text,
-      weight: index === 0 ? 1.0 : 0.8, // set weight 1.0 for the first prompt, 0.8 for others
-    })),
-  });
-  if (response.status !== 200) {
-    throw new Error(`Non-200 response: ${response.statusText}`);
-  }
+const generateImage = async (textPromptsWithWeights, height, width) => {
+  try {
+    const response = await axios.post(`http://localhost:5000/api/generate-image`, {
+      height: height, // height of the output image
+      width: width, // width of the output image
+      cfg_scale: 25, // replace with the desired cfg_scale
+      clip_guidance_preset: 'NONE', // replace with the desired clip_guidance_preset
+      samples: 1, // replace with the desired number of samples
+      steps: 75, // replace with the desired number of steps
+      style_preset: 'digital-art', // replace with the desired style_preset
+      text_prompts: textPromptsWithWeights.map(prompt => ({
+        text: prompt.text,
+        weight: prompt.weight,
+      })),
+    });
+    if (response.status !== 200) {
+      throw new Error(`Non-200 response: ${response.statusText}`);
+    }
 
-  const { image } = response.data;
-  if (!image) {
-    throw new Error('No image data found in the image generation response');
-  }
+    const { image } = response.data;
+    if (!image) {
+      throw new Error('No image data found in the image generation response');
+    }
 
-  // Return the generated image URL or data
-  return image;
+    // Return the generated image URL or data
+    return image;
+  } catch (error) {
+    console.log('Error generating image:', error.message);
+    throw error; // You may also choose to reformat the error before throwing it, if necessary
+  }
 };
 
 export default generateImage;
