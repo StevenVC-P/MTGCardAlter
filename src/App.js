bbb50fa0c648b0ecaa1baa8eb9e68b3b02c43a40
StevenVC-P <77998885+StevenVC-P@users.noreply.github.com>
2023-08-12 22:47:27 -0500

    added sidebar and user prompts

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header.jsx";
 import CardForm from "./components/CardForm";
+import Sidebar from "./components/Sidebar";
 import "./App.css";
 
 const App = () => {
+  const [sidebarText, setSidebarText] = useState("");
+  const [sidebarWeight, setSidebarWeight] = useState(5);
+
   return (
     <div className="container">
       <Header />
-      <CardForm />
+      <div className="content">
+        <Sidebar text={sidebarText} weight={sidebarWeight} setText={setSidebarText} setWeight={setSidebarWeight} />
+        <CardForm sidebarText={sidebarText} sidebarWeight={sidebarWeight} />
+      </div>
     </div>
   );
 };
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+const Sidebar = ({text, weight, setText, setWeight}) => {
+
+    const handleTextInputChange = (event) => {
+        // Only allow characters used in normal writing
+        const pattern = /^[a-zA-Z0-9\s.,!?'"()]*$/;
+        if (pattern.test(event.target.value)) {
+        setText(event.target.value); // using the prop function
+        }
+    };
+
+
+    const handleWeightChange = (event) => {
+        setWeight(Number(event.target.value)); // using the prop function
+    };
+
+
+  return (
+    <div className="sidebar">
+      <label htmlFor="text-input">Enter a custom prompt here:</label>
+      <textarea
+        id="text-input"
+        value={text} // using the prop value
+        onChange={handleTextInputChange}
+        rows="10"
+        cols="30"
+        placeholder="Type here..."
+      />
+      <label htmlFor="weight">Weight:</label>
+      <input
+        type="range"
+        id="weight"
+        name="weight"
+        min="1"
+        max="10"
+        value={weight} // using the prop value
+        onChange={handleWeightChange}
+      />
+      <div>Selected Weight: {weight}</div>
+    </div>
+  );
+};
+
+export default Sidebar;
