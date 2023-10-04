1a112ed353d3e44fbff5ab7153658a46a0f7c224
StevenVC-P <77998885+StevenVC-P@users.noreply.github.com>
2023-10-04 12:57:16 -0500

    Revert "Merge pull request #32 from StevenVC-P/Database"
    This reverts commit 09a5c6fe7cd501b229c1ab2531199948de96fa10, reversing
    changes made to ff0c1e5a07c65479597d92f60d537ac182fdaf00.

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 // server.js
 require("dotenv").config({ path: "../env/.env" });
 const express = require("express");
-const cardRoutes = require("../routes/cards");
 const cors = require("cors");
+const axios = require("axios");
 const app = express();
-
+const request = require("request");
 
 app.use(cors());
 app.use((req, res, next) => {
@@ -20,7 +20,18 @@ const apiHost = process.env.API_HOST;
 const apiKey = process.env.STABILITY_API_KEY;
 const PORT = process.env.PORT || 5000;
 
-app.use("/api/cards", cardRoutes);
+app.get("/api/cards/:cardName", async (req, res) => {
+  try {
+    const cardName = req.params.cardName;
+    const response = await axios.get(`https://api.scryfall.com/cards/named?fuzzy=${cardName}`);
+    if (response.status !== 200) {
+      return res.status(400).json({ message: "Error fetching card details" });
+    }
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching card data" });
+  }
+});
 
 app.post("/api/generate-image", async (req, res) => {
   try {
@@ -65,6 +76,19 @@ app.post("/api/generate-image", async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.get("/proxy-image", async (req, res) => {
+  const url = req.query.url;
+
+  try {
+    const response = await axios.get(url, { responseType: "arraybuffer" });
+
+    res.setHeader("Content-Type", response.headers["content-type"]);
+    res.setHeader("Access-Control-Allow-Origin", "*"); // include the CORS header
+    res.end(Buffer.from(response.data, "binary"));
+  } catch (error) {
+    console.error("Error fetching image:", error);
+    res.status(500).end("Error fetching image");
+  }
 });
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
