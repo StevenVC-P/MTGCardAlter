ed2711572f73d44f501dfacdb77317699564ad91
StevenVC-P <77998885+StevenVC-P@users.noreply.github.com>
2023-10-11 21:33:25 -0500

    updated registration with requirements and redirect

--- src/components/Inputs/EmailLoginForm.jsx
+++ src/components/Inputs/EmailLoginForm.jsx
@@ -1,9 +1,19 @@
 import React, { useState } from 'react';
+import ValidatedInput from './ValidatedInput'; 
 
 const EmailLoginForm = ({ onSuccessfulLogin }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [message, setMessage] = useState('');
+
+    const emailValidator = (email) => {
+    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    return regex.test(email);
+  };
+
+  const passwordValidator = (password) => {
+    return password.length >= 8;
+  };
 
   const emailLogin = async () => {
     try {
@@ -29,8 +39,22 @@ const EmailLoginForm = ({ onSuccessfulLogin }) => {
   return (
     <div>
       <h2>Login with Email</h2>
-      <input type="email" placeholder="Email" onChange={e => setEmail(e.target.value)} />
-      <input type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
+      <ValidatedInput
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        validator={emailValidator}
+        errorMessage="Please enter a valid email."
+      />
+      <ValidatedInput
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        validator={passwordValidator}
+        errorMessage="Password should be at least 8 characters long."
+      />
       <button onClick={emailLogin}>Login</button>
       {message && <p>{message}</p>}
     </div>
