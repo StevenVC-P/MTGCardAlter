725074db9f8ac87514ff970ed6115f168b306789
StevenVC-P <77998885+StevenVC-P@users.noreply.github.com>
2023-12-21 07:01:02 -0600

    added fixes to the UI and banner for loading

---
+++ src/components/MainLayout/LoadingBanner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoadingBanner = () => {
+  return (
+    <div className="loading-banner">
+      <span className="loading-banner-text">Creating Images...</span>
+    </div>
+  );
+};
+
+export default LoadingBanner;
